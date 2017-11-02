604cf5baa42a6330013e832192f376c5af71fdb2
Jordan Leach <jrleach96@gmail.com>
2017-11-02 14:48:33 -0400

    Update Euclid.java

--- Algorithms/Euclid.java
+++ Algorithms/Euclid.java
@@ -8,10 +8,11 @@ public class Euclid {
     int r = 0;
     int gcd = 0;
     boolean done = false;
-
+    System.out.println("We are going to perform Euclid's Algorithm." + "\n"
+    + "Please enter in two value to find their GCD. Or type exit to stop.");
+    
     while(!done){
-      System.out.println("We are going to perform Euclid's Algorithm." + "\n"
-      + "Please enter in two value to find their GCD. Or type exit to stop.");
+     
       Scanner input = new Scanner(System.in);
  
         s = input.next();
@@ -26,6 +27,11 @@ public class Euclid {
             System.out.println("0");
           }
           else {
+            if (m < n) {
+              int temp = n;
+              n = m;
+              m = temp;
+            }
             while (n != 0) {
               r = m % n;
               System.out.println("gcd(" + m + "," + n + ")" + " = " + r);
@@ -38,6 +44,7 @@ public class Euclid {
             gcd = m;
             System.out.print("Greatest common divisor: " + gcd);
             System.out.println("\n");
+            System.out.println("You can put in two new inputs or type exit");
         }
       }
     }
